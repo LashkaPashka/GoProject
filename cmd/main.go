b017e8072098262a8c6fb572b71d22806e4d08d9
root <root@LashkaPashka>
2025-01-19 15:54:50 +0300

    Creation WithTimeout

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"go/project_go/configs"
 	"go/project_go/internal/auth"
@@ -9,9 +10,32 @@ import (
 	"go/project_go/pkg/db"
 	"go/project_go/pkg/middleware"
 	"net/http"
+	"time"
 )
 
 func main(){
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	tick := make(chan struct{})
+	
+	go func () {
+		time.Sleep(4*time.Second)
+		close(tick)
+	}()
+
+
+	select{
+		case <- tick:			
+			fmt.Println("Channel")
+		case <-ctx.Done():
+			fmt.Println("TimeOut") 
+		}
+}
+
+
+
+func main1(){
 	conf := configs.LoadConfig()
 	db := db.NewDb(conf)
 	router := http.NewServeMux()
